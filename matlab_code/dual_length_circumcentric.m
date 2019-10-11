a90d214df2b05806695015e388234d8527866cb4
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-11 19:55:57 +0800

    Create dual_length_circumcentric.m

---
+++ matlab_code/dual_length_circumcentric.m
@@ -0,0 +1,10 @@
+function vertex_dual_length=dual_length_circumcentric(length,area,vertex_next,vertex_prev)
+l0=length;
+l1=length(vertex_next');
+l2=length(vertex_prev');
+l1=l1(:);
+l2=l2(:);
+
+area_temp=[area';area';area'];
+area_temp=area_temp(:);
+vertex_dual_length=(l1.*l1+l2.*l2-l0.*l0).*l0./(8*area_temp);
