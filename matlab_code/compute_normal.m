a6385c8d0a8d8b566b3f0d9cadf05720fb9e3a35
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-11 19:55:18 +0800

    Create compute_normal.m

---
+++ matlab_code/compute_normal.m
@@ -0,0 +1,6 @@
+function normal=compute_normal(points,faces)
+edge1=points(faces(:,2),:)-points(faces(:,1),:);
+edge2=points(faces(:,3),:)-points(faces(:,1),:);
+normal=[edge1(:,2).*edge2(:,3)-edge1(:,3).*edge2(:,2) edge1(:,3).*edge2(:,1)-edge1(:,1).*edge2(:,3) edge1(:,1).*edge2(:,2)-edge1(:,2).*edge2(:,1) ];
+normal_length=sqrt(sum(normal.^2,2));
+normal=-normal./normal_length;
