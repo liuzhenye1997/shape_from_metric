b5dd2ff3f03e0e6538b7f1770eb7dfe132bd31c5
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:14:27 +0800

    Delete quaternion_multiplication.m

--- matlab_code/quaternion_multiplication.m
+++
@@ -1,2 +0,0 @@
-function result=quaternion_multiplication(p,q)
-result=[p(1)*q(1)-p(2)*q(2)-p(3)*q(3) p(1)*q(2)+p(2)*q(1)+p(3)*q(4)-p(4)*q(3) p(1)*q(3)-p(2)*q(4)+p(3)*q(1)+p(4)*q(2) p(1)*q(4)+p(2)*q(3)-p(3)*q(2)+p(4)*q(1)];
