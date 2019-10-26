b636a57920470c43701d5b2614ab2c37dc8f441f
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:12:26 +0800

    Update face_area.m

--- matlab_code/face_area.m
+++ matlab_code/face_area.m
@@ -1,8 +1,5 @@
-function area=face_area(vertex,face)
-length=zeros(size(face,1),3);
-for i=1:3
-    length(:,i)=sqrt(sum((vertex(face(:,mod(i,3)+1),:)-vertex(face(:,mod(i+1,3)+1),:)).^2,2));
-end
+%利用边长求面积
+function area=face_area(length)
 p=sum(length,2)/2;
 area=sqrt(p.*(p-length(:,1)).*(p-length(:,2)).*(p-length(:,3)));
-end
+
