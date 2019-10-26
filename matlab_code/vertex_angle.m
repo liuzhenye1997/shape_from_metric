d663926012a0638b1506a7eaf0154205b077436e
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:15:03 +0800

    Update vertex_angle.m

--- matlab_code/vertex_angle.m
+++ matlab_code/vertex_angle.m
@@ -1,9 +1,10 @@
+%计算mesh的每一个面的三个角度
 function angle=vertex_angle(length,vertex_prev,vertex_next)
 l1=length';
 l2=length(vertex_prev');
 l2=l2(:)';
 lopp=length(vertex_next');
 lopp=lopp(:)';
-size(l1)
+
 c=(l1.^2+l2.^2-lopp.^2)./(2*l1.*l2);
 angle=acos(c);
