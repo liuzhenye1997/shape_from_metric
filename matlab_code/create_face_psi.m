dbaaf5d9880ad5a73c46e5254190aa30f6f00dbb
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:11:55 +0800

    Update create_face_psi.m

--- matlab_code/create_face_psi.m
+++ matlab_code/create_face_psi.m
@@ -1,3 +1,4 @@
+%³õÊ¼Éú³Éface_psi
 function [face_psi1re,face_psi1im,face_psi2re,face_psi2im]=create_face_psi(seed,face_psi1re,face_psi1im,face_psi2re,face_psi2im)
 face_number=size(face_psi1re,1);
 face_psi1re(:)=sin(1:face_number+seed);
