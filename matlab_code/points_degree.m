6d8e5e1bfea4b7392e4e53a91be3466bcb59bc98
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:14:12 +0800

    Update points_degree.m

--- matlab_code/points_degree.m
+++ matlab_code/points_degree.m
@@ -1,6 +1,6 @@
+%求点的度
 function degree=points_degree(face_number,faces)
-
-degree=zeros(face_number,1);
+degree=zeros(max(faces(:)),1);
 for i=1:face_number
     for j=1:3        
         degree(faces(i,j))=degree(faces(i,j))+1;                   
