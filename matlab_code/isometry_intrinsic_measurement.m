21945069676d6b17a4aa283e62136ff78dfba176
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-10-26 15:13:16 +0800

    Update isometry_intrinsic_measurement.m

--- matlab_code/isometry_intrinsic_measurement.m
+++ matlab_code/isometry_intrinsic_measurement.m
@@ -1,14 +1,17 @@
-function [angle,vertex_dual_length,point_area]=isometry_intrinsic_measurement(points,faces,length,vertex_prev,vertex_next,dual_length_type)
-angle=vertex_angle(length,vertex_prev,vertex_next);
-area=face_area(points,faces);
-point_number=size(points,1);
+function [angle,vertex_dual_length,point_area]=isometry_intrinsic_measurement(area,point_number,faces,length,vertex_prev,vertex_next,dual_length_type)
 face_number=size(faces,1);
+%计算每个面的三个角的角度
+angle=vertex_angle(length,vertex_prev,vertex_next);
+
+%计算每个点的面积，定义为其领域三角形之和的1/3。
 point_area=zeros(point_number,1);
 for i=1:face_number
     for j=1:3
         point_area(faces(i,j))=point_area(faces(i,j))+area(i)/3;
     end
 end
+
+%求对偶边的长度。对偶的点选在面的重心或外心
 if strcmp(dual_length_type,'barycentric')
     vertex_dual_length=dual_length_barycentric(length,area);
 else
