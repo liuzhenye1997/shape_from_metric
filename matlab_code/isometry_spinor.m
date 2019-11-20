7d1901d6ee340117418e5a5c27aca9ad82707ecc
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-11-20 20:58:36 +0800

    Update isometry_spinor.m

--- matlab_code/isometry_spinor.m
+++ matlab_code/isometry_spinor.m
@@ -1,18 +1,17 @@
 %初始化face_psi,即λ
-function [face_psi1re,face_psi1im,face_psi2re,face_psi2im]=isometry_spinor(face_number)
-face_psi1re=zeros(face_number,1);
-face_psi1im=zeros(face_number,1);
-face_psi2re=zeros(face_number,1);
-face_psi2im=zeros(face_number,1);
+function lambda=isometry_spinor(face_number)
+lambda=create_face_psi(face_number);
+face_psi_norm=sqrt(sum(lambda.^2,2));
+lambda=lambda./face_psi_norm;
 
-[face_psi1re,face_psi1im,face_psi2re,face_psi2im]=create_face_psi(0,face_psi1re,face_psi1im,face_psi2re,face_psi2im);
-% face_psi1re(:)=1:face_number;
-% face_psi1im(:)=2*(2:face_number++1);
-% face_psi2re(:)=3*(3:face_number+2);
-% face_psi2im(:)=4*(4:face_number+3);
-face_psi=[face_psi1re face_psi1im face_psi2re face_psi2im];
-face_psi_norm=sqrt(sum(face_psi.^2,2));
-face_psi1re=face_psi1re./face_psi_norm;
-face_psi1im=face_psi1im./face_psi_norm;
-face_psi2re=face_psi2re./face_psi_norm;
-face_psi2im=face_psi2im./face_psi_norm;
+%% 初始生成face_psi
+function lambda=create_face_psi(face_number)
+lambda=zeros(face_number,4);
+% lambda(:,1)=sin(1:face_number);
+% lambda(:,2)=sin((1:face_number)*pi*pi);
+% lambda(:,3)=sin((1:face_number)*pi*exp(1));
+% lambda(:,4)=sin((1:face_number)*exp(1));
+lambda(:,1)=rand(face_number,1);
+lambda(:,2)=rand(face_number,1);
+lambda(:,3)=rand(face_number,1);
+lambda(:,4)=rand(face_number,1);
