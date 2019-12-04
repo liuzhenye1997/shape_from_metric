ec8b68d9a963d07ec8b5de6d88f4a0474f31ce7f
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-12-04 14:26:31 +0800

    Update isometry_spinor.m

--- matlab_code/isometry_spinor.m
+++ matlab_code/isometry_spinor.m
@@ -6,11 +6,6 @@ lambda=lambda./face_psi_norm;
 
 %% ³õÊ¼Éú³Éface_psi
 function lambda=create_face_psi(face_number)
-lambda=zeros(face_number,4);
-% lambda(:,1)=sin(1:face_number);
-% lambda(:,2)=sin((1:face_number)*pi*pi);
-% lambda(:,3)=sin((1:face_number)*pi*exp(1));
-% lambda(:,4)=sin((1:face_number)*exp(1));
 lambda(:,1)=rand(face_number,1);
 lambda(:,2)=rand(face_number,1);
 lambda(:,3)=rand(face_number,1);
