57c139e5ba64627a4dcd578cf0d54122e2056030
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-12-16 13:41:21 +0800

    Update main.m

--- matlab_code/main.m
+++ matlab_code/main.m
@@ -1,6 +1,6 @@
 addpath('splsolver')
 max_iteration=40;
-name='torus.obj';
+name='bunny.obj';
 [points_old,faces,~,~]=readObj(name);
 length=edge_length(faces,points_old);
 %g_is_zero令论文中的App D的g直接设为0.以运行40次为例，误差为10^-10量级.想省点时间设g=0，即g_is_zero=true.  
@@ -18,7 +18,7 @@ mesh.points=fix_rotation(mesh.f_hedge,points_old,mesh.v_dst,mesh.v_src,mesh.v_fa
 save_obj(mesh.points,mesh.faces,sprintf('BunnyFromMetric_%d.obj',max_iteration));
 %测试输出结果，从左到右是相对误差取绝对值后的最大相对误差，最大的相对误差，最小的相对误差，l2误差，最大的对数相对误差，最小的对数相对误差。
 [max_abs_rel_err,min_rel_err,max_rel_err,l2_rel_err,max_log_err,min_log_err]=evaluate(mesh.f_number,mesh.v_dst,mesh.v_src,mesh.points,mesh.length);
-
+fprintf("最大相对误差为%f,L2误差为%f",max_abs_rel_err,l2_rel_err);
 
  
 
