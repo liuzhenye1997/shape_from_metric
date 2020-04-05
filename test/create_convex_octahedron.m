589068a0d5096121f4d96381b39e54468d710887
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2020-04-05 23:37:58 +0800

    Update create_convex_octahedron.m

--- test/create_convex_octahedron.m
+++ test/create_convex_octahedron.m
@@ -1,4 +1,6 @@
-function points_sol=create_convex_octahedron()
+%创建一个16面体，由上下两个顶点和中间4个顶点组成
+%z用来控制上下两个顶点到x-y平面的距离，距离为rand(1)*z+1
+function points_sol=create_convex_octahedron(z)
 points_sol=zeros(6,3);
 points_sol(1,1)=-(rand(1)+1);
 points_sol(1,3)=(rand(1)-0.5)*2;
@@ -12,6 +14,6 @@ points_sol(3,2)=(rand(1)-0.5)*2;
 points_sol(4,2)=rand(1)+1;
 points_sol(4,3)=(rand(1)-0.5)*2;
 points_sol(4,1)=(rand(1)-0.5)*2;
-points_sol(5,3)=rand(1)+1;
-points_sol(6,3)=-(rand(1)+1);
+points_sol(5,3)=rand(1)*z+1;
+points_sol(6,3)=-(rand(1)*z+1);
 
